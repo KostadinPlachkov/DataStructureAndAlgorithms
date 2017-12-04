9ac564fc67cfa2ece185934cca9d32dbe3996aa9
KostadinPlachkov <kostt23@gmail.com>
2017-12-04 19:31:00 +0200

    Fixed the SortEngine project hierarchy.

--- SortEngine/src/main/java/Main.java
+++ SortEngine/src/main/java/Main.java
@@ -1,16 +1,5 @@
 import java.util.List;
 
-/**
- * Write a program  having generic functionality for sorting objects,
- all are instance of  java.lang.Number and Comparable java classes
- by two different ways – bubble and (quick) sortExample
-
- SortEngine<Float> sortEngine;
- sortEngine.add(num1)..
- List<Float> sorted = sortEngine.bubbleSort()/(quickSort())
- */
-
-
 
 public class Main {
     public static void main(String[] args) throws Exception {
